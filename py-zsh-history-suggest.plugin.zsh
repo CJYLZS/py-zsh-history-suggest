e5a77a460e803b877c472be22fc0c3bcd70c8e8c
CJYLZS <691086891@qq.com>
2022-10-30 10:12:40 +0800

    update plugin

--- py-zsh-history-suggest.plugin.zsh
+++ py-zsh-history-suggest.plugin.zsh
@@ -1,11 +1,11 @@
 
 
-function test(){
+function py_zsh_history_suggest(){
     BUFFER=$(LINES=${LINES} COLUMNS=${COLUMNS} /usr/bin/suggest)
     CURSOR=$#BUFFER
     zle -R -c
 }
 
-zle -N test
+zle -N py_zsh_history_suggest
 
-bindkey '^R' test
+bindkey '^R' py_zsh_history_suggest
